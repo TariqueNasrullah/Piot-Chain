681177529a0b45c26dd0f7d62e4c78795fbe2273
TariqueNasrullah <nasrullahtarique@gmail.com>
2020-05-28 20:11:04 +0600

    Key implemented

---
+++ key.go
@@ -0,0 +1,95 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+// Key structure
+type Key struct {
+	PrivateKey *ecdsa.PrivateKey
+	PublicKey  []byte
+	SecretKey  []byte
+	Token      []byte
+}
+
+// GenerateKey generates key
+func GenerateKey(keyPath string) (*Key, error) {
+	if _, err := os.Stat(keyPath); os.IsNotExist(err) {
+		var key Key
+
+		// Assign SecretKey
+		var randomKey [32]byte
+		rand.Read(randomKey[:])
+		key.SecretKey = randomKey[:]
+
+		// Gnerate ecdsa priv pub key
+		privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		if err != nil {
+			return &key, err
+		}
+		key.PrivateKey = privKey
+		key.PublicKey = append(privKey.PublicKey.X.Bytes(), privKey.PublicKey.Y.Bytes()...)
+
+		return &key, nil
+	}
+	return &Key{}, errors.New("Key Exists")
+}
+
+// LoadKey loads key from file
+func LoadKey(keyPath string) (*Key, error) {
+	if _, err := os.Stat(keyPath); os.IsNotExist(err) {
+		return &Key{}, errors.New("Key File Does not exist")
+	}
+
+	var key Key
+
+	fileContent, err := ioutil.ReadFile(keyPath)
+	if err != nil {
+		return &key, err
+	}
+
+	gob.Register(elliptic.P256())
+	decoder := gob.NewDecoder(bytes.NewReader(fileContent))
+	err = decoder.Decode(&key)
+	if err != nil {
+		return &key, err
+	}
+
+	return &key, nil
+}
+
+// SaveFile saves key into file
+func (key *Key) SaveFile(keyPath string) {
+	var buffer bytes.Buffer
+	enc := gob.NewEncoder(&buffer)
+	err := enc.Encode(key)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = ioutil.WriteFile(keyPath, buffer.Bytes(), 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func (key *Key) String() string {
+	var lines []string
+
+	lines = append(lines, fmt.Sprintf(" --------- Device Key Information:"))
+	lines = append(lines, fmt.Sprintf(" PubKey     : %x", key.PublicKey))
+	lines = append(lines, fmt.Sprintf(" SecretKey  : %x", key.SecretKey))
+	lines = append(lines, fmt.Sprintf(" Token      : %x", key.Token))
+
+	return strings.Join(lines, "\n")
+}
