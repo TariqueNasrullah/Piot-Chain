58d1512bbadf540fd547279b887334b55ef71d50
TariqueNasrullah <nasrullahtarique@gmail.com>
2020-05-29 02:43:14 +0600

    main helper

--- main.go
+++ main.go
@@ -1,12 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"fmt"
 
-// Add adds two number
-func Add(a, b int) int {
-	return a + b
+	"github.com/TariqueNasrullah/Private-IoT-blockchain/blockchain"
+)
+
+var (
+	keyPath = "key.data"
+)
+
+func generateToken(username, password string) ([]byte, error) {
+	hash := []byte(username + password)
+
+	key, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	r, s, err := ecdsa.Sign(rand.Reader, key, hash[:])
+	if err != nil {
+		return []byte{}, err
+	}
+
+	signature := append(r.Bytes(), s.Bytes()...)
+	return signature, nil
+}
+func handle(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	fmt.Println(Add(10, 20))
+	token, err := generateToken("admin", "pass")
+	handle(err)
+	key, err := blockchain.GenerateKey(keyPath)
+	handle(err)
+	key.Token = token
+
+	trans := blockchain.Transaction{
+		Data: []byte("Hello Transaction"),
+	}
+	block := blockchain.Block{
+		Transactions: []*blockchain.Transaction{&trans},
+		Token:        key.Token,
+		PublicKey:    key.PublicKey,
+	}
+
+	err = block.Sign(key.PrivateKey)
+	handle(err)
+
+	pow := blockchain.NewProof(&block)
+	nonce, hash := pow.Run()
+	block.Nonce = nonce
+	block.Hash = hash
+
+	fmt.Printf("%s\n", block)
+
+	validSig := block.VerifySignature()
+	fmt.Printf("Signature Verification: %v\n", validSig)
 }
