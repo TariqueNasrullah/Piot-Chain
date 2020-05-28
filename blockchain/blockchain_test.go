814a0d30a0ef57f03625e863727d55bd568a75b1
TariqueNasrullah <nasrullahtarique@gmail.com>
2020-05-28 21:06:14 +0600

    key testing added

--- blockchain/blockchain_test.go
+++ blockchain/blockchain_test.go
@@ -2,6 +2,7 @@ package blockchain
 
 import (
 	"bytes"
+	"os"
 	"testing"
 )
 
@@ -31,5 +32,26 @@ func TestSerializeDeserialize(t *testing.T) {
 
 	if !bytes.Equal(block.Transactions[0].Data, blockPrime.Transactions[0].Data) {
 		t.Fatal("Two block are not same")
+	}
+}
+
+func TestKey(t *testing.T) {
+	keyPath := "key.data"
+
+	defer func() {
+		if _, err := os.Stat(keyPath); os.IsNotExist(err) {
+			return
+		}
+		os.Remove(keyPath)
+	}()
+
+	key, err := GenerateKey(keyPath)
+	if err != nil {
+		t.Fatalf("Error Not expected! Error: %v\n", err)
+	}
+
+	err = key.SaveFile(keyPath)
+	if err != nil {
+		t.Fatalf("Error Not expected! Error: %v\n", err)
 	}
 }
