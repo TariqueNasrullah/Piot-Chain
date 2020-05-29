025014f5e6145e245739cf34bb9a9c5fd34be44c
TariqueNasrullah <nasrullahtarique@gmail.com>
2020-05-29 07:20:16 +0600

    db exist implementation

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,8 +1,20 @@
 package blockchain
 
-import "github.com/dgraph-io/badger"
+import (
+	"os"
+
+	"github.com/dgraph-io/badger"
+)
 
 // BlockChain structure
 type BlockChain struct {
 	Database *badger.DB
+}
+
+// DBexists checks if database file already exists
+func DBexists(dbFile string) bool {
+	if _, err := os.Stat(dbFile); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
