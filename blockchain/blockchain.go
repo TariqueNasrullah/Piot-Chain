49e9ac133d96d2922e49a00d0a860784ec006687
TariqueNasrullah <nasrullahtarique@gmail.com>
2020-05-29 08:20:22 +0600

    addblock feature implemented

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,6 +1,9 @@
 package blockchain
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/dgraph-io/badger"
 )
 
@@ -22,4 +25,74 @@ func InitBlockChain(dbPath string) (*BlockChain, error) {
 	chain := BlockChain{Database: db}
 
 	return &chain, nil
+}
+
+// AddBlock adds a block to the chain
+// 1. chekcs signature
+// 2. checks pow
+// 3. checks existance of previous hash
+func (chain *BlockChain) AddBlock(block *Block) *ChainError {
+	valid := block.VerifySignature()
+	if !valid {
+		return &ChainError{
+			StatusCode: ErrorInvalidSignature,
+			Err:        errors.New("Signature can't be verified"),
+		}
+	}
+
+	pow := NewProof(block)
+	valid = pow.Validate()
+	if !valid {
+		return &ChainError{
+			StatusCode: ErrorInvalidProofOfWork,
+			Err:        errors.New("Invalid proof of work"),
+		}
+	}
+
+UpdateDB:
+	err := chain.Database.Update(func(txn *badger.Txn) error {
+		_, err := txn.Get(block.PrevHash)
+		if err == badger.ErrKeyNotFound {
+			return err
+		}
+
+		data, err := block.Serialize()
+		if err != nil {
+			return err
+		}
+
+		err = txn.Set(block.Hash, data)
+		if err != nil {
+			return err
+		}
+
+		address, err := block.Address()
+		if err != nil {
+			return err
+		}
+
+		err = txn.Set(address, block.Hash)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err == badger.ErrConflict {
+		goto UpdateDB
+	}
+	if err == badger.ErrKeyNotFound {
+		return &ChainError{
+			StatusCode: ErrorPreviousHashNotFound,
+			Err:        errors.New("Previous hash not found"),
+		}
+	}
+	if err != nil {
+		return &ChainError{
+			StatusCode: ErrorUnknown,
+			Err:        fmt.Errorf("Error: %v", err),
+		}
+	}
+	return nil
 }
